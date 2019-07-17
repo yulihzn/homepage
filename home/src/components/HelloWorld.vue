8a2398a8982d6f2bc17ebb0ff69ca0ad3cf0c6de
yulihzn <330193303@qq.com>
2019-07-18 00:30:12 +0800

    fix

--- home/src/components/HelloWorld.vue
+++ home/src/components/HelloWorld.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="main-container">
+    <h1 class="h3">{{ msg }}</h1>
+    <div class="bottom-container">
     <a class="game" href="https://banditcatstudio.com/web-mobile/index.html"><img src=".././assets/gamepad.png"/></a>
+    <a class="wine" href="https://banditcatstudio.com/dist/index.html"><img src=".././assets/wine.png"/></a>
+    </div>
   </div>
 </template>
 
@@ -17,7 +20,10 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h3 {
-  margin: 40px 0 0;
+  position: relative;
+  top: 50%;
+  height: 100%;
+  align-content: center;
 }
 ul {
   list-style-type: none;
@@ -31,7 +37,28 @@ a {
   color: #42b983;
 }
 .game{
+  display: inline;
+  position: relative;
+  margin: 32px;
+}
+.wine{
+  display: inline;
+  position: relative;
+  margin: 32px;
+}
+.bottom-container{
   position: fixed;
+  display: flex;
+  width: 100%;
+  justify-content: center;
   bottom: 32px;
+}
+.main-container{
+  position: fixed;
+  display: flex;
+  top: 30%;
+  height: 100%;
+  width: 100%;
+  justify-content: center;
 }
 </style>
